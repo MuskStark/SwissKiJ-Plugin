c2bc0a35c86ac24f19281bae59150b352f83654f
MuskStark <gitman@outlook.sg>
2026-06-27 22:17:37 +0800

    chore: add maven-env.sh helper for IntelliJ-bundled Maven

---
+++ scripts/maven-env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# IntelliJ-bundled Maven + JDK 21 env. Source this: source scripts/maven-env.sh
+export JAVA_HOME="/Users/phoebej/Library/Java/JavaVirtualMachines/azul-21.0.11/Contents/Home"
+export MVN="/Users/phoebej/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven3/bin/mvn"
+export PATH="$JAVA_HOME/bin:$PATH"
